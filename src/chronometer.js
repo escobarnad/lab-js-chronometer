3c37957eea1fa1b28135723e5de32c52b100672f
Nadia Escobar <escobarnad@gmail.com>
2024-03-15 18:16:46 +0100

    Bonus not working

--- src/chronometer.js
+++ src/chronometer.js
@@ -3,16 +3,26 @@ class Chronometer {
     // ... your code goes here
     this.currentTime = 0;
     this.intervalId = null;
+    this.currentMilliseconds = 0;
+    this.millisecondsIntervalId = 0
   }
 
-  start(printTimeCallback) {
+  start(printTimeCallback, printMilliseconds) {
     // ... your code goes here
     this.intervalId = setInterval(() => {
-      this.currentTime +=1
+      this.currentTime += 1
       if (printTimeCallback){
         printTimeCallback(this.currentTime)
       }
     }, 1000)
+
+    this.millisecondsIntervalId = setInterval( () => {
+      if (this.currentMilliseconds === 99) {
+        this.currentMilliseconds = 0;
+      }
+      this.currentMilliseconds += 1;
+      if (printMilliseconds) printMilliseconds();
+    }, 10);
   }
 
   getMinutes() {
@@ -38,14 +48,27 @@ class Chronometer {
   stop() {
     // ... your code goes here
     clearInterval(this.intervalId)
+    clearInterval(this.millisecondsIntervalId);
   }
 
   reset() {
     // ... your code goes here
     this.currentTime = 0
+    this.currentMilliseconds = 0;
   }
 
   split() {
     // ... your code goes here
+    let minutes = this.computeTwoDigitNumber(this.getMinutes());
+    let seconds = this.computeTwoDigitNumber(this.getSeconds());
+    let milliseconds = this.computeTwoDigitNumber(this.currentMilliseconds);
+
+    // Store current milliseconds
+    let currentMillis = this.currentMilliseconds;
+
+    // Calculate the milliseconds part using the stored value
+    milliseconds = this.computeTwoDigitNumber(currentMillis);
+
+    return `${minutes}:${seconds}:${milliseconds}`;
   }
 }
