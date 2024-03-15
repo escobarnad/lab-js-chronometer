eecbf527c18dcfae613dc80634a67385f48ab3a8
Nadia Escobar <escobarnad@gmail.com>
2024-03-15 17:56:07 +0100

    Finished reset()

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,38 +1,48 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
-
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
-
-
+    this.intervalId = setInterval(() => {
+      this.currentTime +=1
+      if (printTimeCallback){
+        printTimeCallback(this.currentTime)
+      }
+    }, 1000)
   }
 
   getMinutes() {
     // ... your code goes here
-
+    return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
     // ... your code goes here
-
+    return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
-
+    console.log(value)
+    if (value.toString().length === 2){
+      return value.toString()
+    } else {
+      return `0${value}`
+    }
   }
 
   stop() {
     // ... your code goes here
-
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
-
+    this.currentTime = 0
   }
 
   split() {
